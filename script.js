91038ab03253a91abf3934301047bc964d4bc49d
jc <jchorzempa@posteo.org>
2022-08-22 07:51:05 +0200

    Add functionality for addition operations

--- script.js
+++ script.js
@@ -8,7 +8,9 @@ const commaBtn = document.getElementById('comma');
 const equalsBtn = document.getElementById('equals');
 const delimiter = '_';
 const operatingArray = [];
+const ongoingArray = [];
 let joinedArray;
+let newJoinedArray;
 
 function addDisplayNumber(number) {
 	display.textContent += number;
@@ -23,11 +25,24 @@ function clearDisplay() {
 		operatingArray.length = 0;
 	}
 }
+
+function deleteDigit() {
+	display.textContent = display.textContent.toString().slice(0, -1);
+	operatingArray.pop();
+}
+
+deleteBtn.addEventListener('click', deleteDigit);
+
+window.addEventListener('keydown', keyboardInput);
+
+function keyboardInput(e) {
+	if (e.key >= 0 && e.key <= 9) addDisplayNumber(e.key);
+}
+
 digits.forEach((digit) => {
 	digit.addEventListener('click', function () {
 		addDisplayNumber(digit.textContent); //show number on screen
 		operatingArray.push(digit.textContent); //adds number to an array
-		joinedArray = operatingArray.join('');
 	});
 });
 operators.forEach((operator) => {
@@ -39,6 +54,7 @@ operators.forEach((operator) => {
 clearBtn.addEventListener('click', clearDisplay);
 
 function operate() {
+	joinedArray = operatingArray.join('');
 	let justNumbers = joinedArray.split(delimiter).map((item) => parseInt(item));
 	let a = justNumbers[0];
 	let b = justNumbers[1];
@@ -52,6 +68,47 @@ function operate() {
 	} else if (display.textContent.includes(' / ')) {
 		display.textContent = a / b;
 	}
+
+	operatingArray.length = 0;
+	operatingArray.push(display.textContent);
+
+	// while (display.textContent === true) {
+	// 	digits.forEach((digit) => {
+	// 		digit.addEventListener('click', function () {
+	// 			addDisplayNumber(digit.textContent); //show number on screen
+	// 			ongoingArray.push(digit.textContent); //adds number to an array
+	// 			newJoinedArray = ongoingArray.join('');
+	// 			console.log(newJoinedArray);
+	// 		});
+	// 	});
+	// 	operators.forEach((operator) => {
+	// 		operator.addEventListener('click', function () {
+	// 			addDisplayOperator(operator.textContent); //show number on screen
+	// 			ongoingArray.push(delimiter); //adds number to an array
+	// 			console.log(ongoingArray);
+	// 		});
+	// 	});
+
+	// 	let newNumbers = newJoinedArray.split(delimiter).map((item) => parseInt(item));
+	// 	let x = newNumbers[0];
+	// 	let y = newNumbers[1];
+
+	// 	if (display.textContent.includes(' - ')) {
+	// 		display.textContent = x - y;
+	// 		ongoingArray.push(display.textContent);
+	// 	} else if (display.textContent.includes(' + ')) {
+	// 		display.textContent = x + y;
+	// 	} else if (display.textContent.includes(' x ')) {
+	// 		display.textContent = x * y;
+	// 	} else if (display.textContent.includes(' / ')) {
+	// 		display.textContent = x / y;
+	// 	}
+
+	// while (display.textContent === true)
+
+	// addDisplayOperator
+
+	// ongoingArray.push(display.textContent);
 }
 
 equalsBtn.addEventListener('click', operate);
