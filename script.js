851b3ec7fddaf54b5589b2a8b76a64581f80e372
jc <jchorzempa@posteo.org>
2022-08-22 08:19:59 +0200

    Add math functions as per instructions

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ const commaBtn = document.getElementById('comma');
 const equalsBtn = document.getElementById('equals');
 const delimiter = '_';
 const operatingArray = [];
-const ongoingArray = [];
 let joinedArray;
-let newJoinedArray;
 
 function addDisplayNumber(number) {
 	display.textContent += number;
@@ -18,6 +16,7 @@ function addDisplayNumber(number) {
 function addDisplayOperator(sign) {
 	display.textContent += sign;
 }
+
 function clearDisplay() {
 	if (display.textContent === '') return 0;
 	else {
@@ -25,19 +24,56 @@ function clearDisplay() {
 		operatingArray.length = 0;
 	}
 }
-
 function deleteDigit() {
 	display.textContent = display.textContent.toString().slice(0, -1);
 	operatingArray.pop();
+}
+function keyboardInput(e) {
+	if (e.key >= 0 && e.key <= 9) addDisplayNumber(e.key);
+}
+function roundResult(number) {
+	return Math.round(number * 1000) / 1000;
+}
+
+function operate() {
+	joinedArray = operatingArray.join('');
+	let justNumbers = joinedArray.split(delimiter).map((item) => parseFloat(item));
+	let a = justNumbers[0];
+	let b = justNumbers[1];
+
+	if (display.textContent.includes(' - ')) {
+		display.textContent = roundResult(subtract(a, b));
+	} else if (display.textContent.includes(' + ')) {
+		display.textContent = roundResult(add(a, b));
+	} else if (display.textContent.includes(' x ')) {
+		display.textContent = roundResult(multiply(a, b));
+	} else if (display.textContent.includes(' / ')) {
+		display.textContent = roundResult(divide(a, b));
+	}
+	operatingArray.length = 0;
+	operatingArray.push(display.textContent);
+}
+
+function add(a, b) {
+	return a + b;
+}
+function subtract(a, b) {
+	return a - b;
+}
+function multiply(a, b) {
+	return a * b;
+}
+function divide(a, b) {
+	return a / b;
 }
 
 deleteBtn.addEventListener('click', deleteDigit);
 
 window.addEventListener('keydown', keyboardInput);
 
-function keyboardInput(e) {
-	if (e.key >= 0 && e.key <= 9) addDisplayNumber(e.key);
-}
+clearBtn.addEventListener('click', clearDisplay);
+
+equalsBtn.addEventListener('click', operate);
 
 digits.forEach((digit) => {
 	digit.addEventListener('click', function () {
@@ -51,67 +87,11 @@ operators.forEach((operator) => {
 		operatingArray.push(delimiter); //adds number to an array
 	});
 });
-clearBtn.addEventListener('click', clearDisplay);
 
-function operate() {
-	joinedArray = operatingArray.join('');
-	let justNumbers = joinedArray.split(delimiter).map((item) => parseInt(item));
-	let a = justNumbers[0];
-	let b = justNumbers[1];
-
-	if (display.textContent.includes(' - ')) {
-		display.textContent = a - b;
-	} else if (display.textContent.includes(' + ')) {
-		display.textContent = a + b;
-	} else if (display.textContent.includes(' x ')) {
-		display.textContent = a * b;
-	} else if (display.textContent.includes(' / ')) {
-		display.textContent = a / b;
-	}
-
-	operatingArray.length = 0;
-	operatingArray.push(display.textContent);
-
-	// while (display.textContent === true) {
-	// 	digits.forEach((digit) => {
-	// 		digit.addEventListener('click', function () {
-	// 			addDisplayNumber(digit.textContent); //show number on screen
-	// 			ongoingArray.push(digit.textContent); //adds number to an array
-	// 			newJoinedArray = ongoingArray.join('');
-	// 			console.log(newJoinedArray);
-	// 		});
-	// 	});
-	// 	operators.forEach((operator) => {
-	// 		operator.addEventListener('click', function () {
-	// 			addDisplayOperator(operator.textContent); //show number on screen
-	// 			ongoingArray.push(delimiter); //adds number to an array
-	// 			console.log(ongoingArray);
-	// 		});
-	// 	});
-
-	// 	let newNumbers = newJoinedArray.split(delimiter).map((item) => parseInt(item));
-	// 	let x = newNumbers[0];
-	// 	let y = newNumbers[1];
-
-	// 	if (display.textContent.includes(' - ')) {
-	// 		display.textContent = x - y;
-	// 		ongoingArray.push(display.textContent);
-	// 	} else if (display.textContent.includes(' + ')) {
-	// 		display.textContent = x + y;
-	// 	} else if (display.textContent.includes(' x ')) {
-	// 		display.textContent = x * y;
-	// 	} else if (display.textContent.includes(' / ')) {
-	// 		display.textContent = x / y;
-	// 	}
-
-	// while (display.textContent === true)
-
-	// addDisplayOperator
-
-	// ongoingArray.push(display.textContent);
-}
-
-equalsBtn.addEventListener('click', operate);
+commaBtn.addEventListener('click', function () {
+	addDisplayOperator(commaBtn.textContent); //show number on screen
+	operatingArray.push(commaBtn.textContent); //adds number to an array
+});
 
 // function operate(operator, a, b) {
 // a = Number(a);
@@ -128,20 +108,4 @@ equalsBtn.addEventListener('click', operate);
 // 	default:
 // 		return null;
 // }
-// }
-
-// function add(a, b) {
-// 	return a + b;
-// }
-
-// function subtract(a, b) {
-// 	return a - b;
-// }
-
-// function multiply(a, b) {
-// 	return a * b;
-// }
-
-// function divide(a, b) {
-// 	return a / b;
 // }
